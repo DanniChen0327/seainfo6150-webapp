a742941db757044a2b46d12b4085052e0a45cd46
Danni Chen <chendanni@chendannideMacBook-Pro.local>
2019-11-04 14:28:06 -0800

    assignment week-9 finished

--- src/ArticleList/ArticleListItem.jsx
+++ src/ArticleList/ArticleListItem.jsx
@@ -11,19 +11,21 @@ const ArticleListItem = props => (
         <html lang="en">
             <head>
                 <meta charset="UTF-8" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-                <title>Week6 assignment</title>
+                <link rel="stylesheet" href="ArticleList.module.css"></link>
+                <title>Week9 assignment</title>
             </head>
 
             <body >
                 <ArticleImage article = {props.article}/>
-                <SlugButton article = {props.article}/>
+                
                 <section id = "container">
+
                     <h1 class = "item-h1">{props.article.title}</h1> 
                     <p class = "item-p">{props.article.shortText}</p>
                     <time class = "item -time" dateTime = {props.article.pubDate}>{props.article.pubDate}</time>
-                    
+                    <SlugButton article = {props.article}/>
                 </section>
                 
             </body>
